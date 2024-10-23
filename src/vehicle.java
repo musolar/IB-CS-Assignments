843608a8ce6fe0c8d0081a47a5a0132fcf054a08
musolar <ethan.tonini@outlook.com>
2024-10-23 15:24:44 -0600

    Added more examples
    Co-authored-by: Erick  <749733@dpsk12.net>

--- src/vehicle.java
+++ src/vehicle.java
@@ -1,9 +1,9 @@
-public class vehicle {
+public class Vehicle {
     public String Colour;
     public String Type;
     public int Engine;
 
-    public vehicle(String Colour, String Type, int Engine) {
+    public Vehicle(String Colour, String Type, int Engine) {
         this.Colour = Colour;
         this.Type = Type;
         this.Engine = Engine;
